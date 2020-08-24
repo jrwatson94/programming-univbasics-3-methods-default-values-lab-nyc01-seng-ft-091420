06fbb8caf3d96ca3ecb4a816bc5bb24e1b71ed1e
Reid Watson <j.watsonreid@gmail.com>
2020-08-24 23:32:15 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,7 @@
 
 def meal_choice (veg1,veg2,protein = "tofu")
   puts "What a nutritious meal!"
-  message = puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  message = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts message
   return message
 end
